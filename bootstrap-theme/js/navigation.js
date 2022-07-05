b3ac995b56d1ca236035eeef81d4a4da393ed62c
vac9p222 <75167383+vac9p222@users.noreply.github.com>
2022-07-05 19:34:30 +0700

    v1

---
+++ bootstrap-theme/js/navigation.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($) {
+
+	$('.slider-product').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		centerMode: true,
+		arrows: false,
+		fade: true,	
+		dots: true	
+	  });
+
+
+  })
+  
